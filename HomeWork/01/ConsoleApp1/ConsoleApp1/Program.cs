06bacf485768cb01063cca2e01b78a2673dc68c2
Vladimir <vovakurlichenko@gmail.com>
2019-09-20 17:43:31 +0300

    Ready

---
+++ HomeWork/01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя:");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Здравствуйте, {name}");
+            Thread.Sleep(5000);
+            Console.WriteLine($"До свидания, {name}");
+            Console.ReadKey();
+        }
+    }
+}
