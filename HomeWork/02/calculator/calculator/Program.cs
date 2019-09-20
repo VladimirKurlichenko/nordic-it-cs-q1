845586e738402d21d6ba3c1c88caef53a914e8c8
Vladimir <vovakurlichenko@gmail.com>
2019-09-20 17:36:38 +0300

    Ready

---
+++ HomeWork/02/calculator/calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа:\n");
+
+            Console.WriteLine("Введите переменную x ");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите переменную y ");
+            var y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nРезультат вычислений:");
+            Console.WriteLine($"{x} + {y} = {x + y}");
+
+            Console.WriteLine($"{x} - {y} = {x - y}");
+
+            Console.WriteLine($"{x} * {y} = {x * y}");
+
+        }
+    }
+}
---
+++ HomeWork/02/calculatorV2/calculatorV2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace calculatorV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа: \n");
+
+            Console.WriteLine("Введите переменную x ");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите переменную y ");
+            var y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nВыберите операцию: \n" + "+ - Сложение\n" + "- - Вычитание\n" + "* - Умножение\n" + 
+                              "/ - Деление\n" + "% - Остаток от деления\n" + "^ - Возвести в степень\n");
+
+            Console.WriteLine("Ваш выбор: ");
+            string op = Console.ReadLine();
+            Console.WriteLine("\nРезультат: ");
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine($"{x} + {y} = {x + y}");
+                    break;
+
+                case "-":
+                    Console.WriteLine($"{x} - {y} = {x - y}");
+                    break;
+
+                case "*":
+                    Console.WriteLine($"{x} * {y} = {x * y}");
+                    break;
+
+                case "/":
+                    Console.WriteLine($"{x} / {y} = {x / y}");
+                    break;
+
+                case "%":
+                    Console.WriteLine($"{x} % {y} = {x % y}");
+                    break;
+
+                case "^":
+                    Console.WriteLine($"{x} ^ {y} = {Math.Pow(x, y)}");
+                    break;
+
+            }
+        }
+    }
+}
