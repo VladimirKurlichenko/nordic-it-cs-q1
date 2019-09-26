74d7226262cbb9eae8e4ff7d8eb2724a910b1c5e
Vova K <vovakurlichenko@gmail.com>
2019-09-26 01:10:56 -0700

    update

--- HomeWork/03/NameAge/NameAge/Program.cs
+++ HomeWork/03/NameAge/NameAge/Program.cs
@@ -4,36 +4,79 @@ namespace NameAge
 {
     class Program
     {
-        static string Input(string[] inputDate, string valueType)
+        static bool CheckName(string[] testName, int index)
+        {
+            if (string.IsNullOrWhiteSpace(testName[index]))
+            {
+                return true;
+            }
+            return false;
+        }
+        static string EnterName(string[] inputName)
         {
             
-            for (int i = 0; i < inputDate.Length; i++)
+            for (int i = 0; i < inputName.Length; i++)
             {
-                int numbering = i+1;
-                Console.Write($"Enter {numbering} {valueType}: ");
-                inputDate[i] = Console.ReadLine();
+                while(CheckName(inputName, i))
+                {
+                    Console.Write($"Enter {i + 1} name: ");
+                    inputName[i] = Console.ReadLine();
+
+                    if(CheckName(inputName, i))
+                    {
+                        Console.WriteLine("NO NAME ENTERED!!!!");
+                    }
+                   
+                }
             }
             return null;
         }
-        static void Output(string[] first, string[] second)
+
+        static bool CheckAge(int[] testAge, int mark)
+        {
+            if(testAge[mark] <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        static string EnterAge(int[] inputAge)
+        {
+
+            for (int i = 0; i < inputAge.Length; i++)
+            {
+                while (CheckAge(inputAge, i))
+                {
+                    Console.Write($"Enter {i + 1} age: ");
+                    int.TryParse(Console.ReadLine(), out inputAge[i]);
+
+                    if (CheckAge(inputAge, i))
+                    {
+                        Console.WriteLine("NO NAME ENTERED!!!!");
+                    }
+
+                }
+            }
+            return null;
+        }
+
+        static void WriteResult(string[] first, int[] second)
         {
             for(int i = 0; i < first.Length; i++)
             {
-                Console.Write($"Name: {first[i]}, age: {second[i]}.\n");
+                Console.Write($"Name: {first[i]}, age in 4 years: {second[i] + 4}.\n");
             }
         }
 
         static void Main(string[] args)
         {
             string[] name = new string[3];
-            string typeName = "Name";
-            Input(name, typeName);
+            EnterName(name);
 
-            string typeAge = "Age";
-            string[] age = new string[3];
-            Input(age, typeAge);
+            int[] age = new int[3];
+            EnterAge(age);
 
-            Output(name, age);
+            WriteResult(name, age);
 
             Console.ReadKey();
         }
