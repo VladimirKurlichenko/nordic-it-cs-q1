7b92565fa614f42d553b3def3e6105f7c34eb320
Vladimir <vovakurlichenko@gmail.com>
2019-10-01 13:52:34 +0300

    update

---
+++ HomeWork/04/ColorSelection/ColorSelection/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ColorSelection
+{
+    [Flags]
+    enum Colors
+    {
+        Black = 1,
+        Blue = 2,
+        Cyan = 4,
+        Grey = 8,
+        Green = 16,
+        Magenta = 32,
+        Red = 64,
+        White = 128,
+        Yellow = 256
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Color list: \n");
+
+            
+            for (int i = 0; i < 9; i++)
+            {
+                double index =Math.Pow(2, i);
+                Console.WriteLine($"{(Colors)index} - {index}");
+            }
+
+            Colors favoriteColors = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"\nChoose {i+1} color you like: ");
+                favoriteColors = favoriteColors | (Colors)int.Parse(Console.ReadLine());
+            }
+
+            Colors noFavoriteColors = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                double mark = Math.Pow(2, i);
+                if ((favoriteColors & (Colors)mark) != (Colors)mark)
+                {
+                    noFavoriteColors = noFavoriteColors | (Colors)mark; 
+                }
+            }
+
+            Console.WriteLine($"Favorite colors: {favoriteColors}\nUnlived colors: {noFavoriteColors}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork/04/Juice/Juice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juice
+{
+    [Flags]
+    enum Containers
+    {
+        TwentyLiters = 0x1,
+        FiveLiters = 0x1 << 1,
+        OneLiters = 0x1 << 2
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much juice (in liters) do I need to pack?");
+            double litersJuice = double.Parse(Console.ReadLine());
+
+            var counterContainers = new int[3];
+
+            counterContainers[0] = (int)Math.Floor(litersJuice / 20d);
+            litersJuice -= 20 * counterContainers[0];
+
+            counterContainers[1] = (int)Math.Floor(litersJuice / 5d);
+            litersJuice -= 5 * counterContainers[1];
+            counterContainers[2] = (int)Math.Ceiling((litersJuice / 1d));
+            var container =(Containers[])Enum.GetValues(typeof(Containers));
+            for (int i = 0; i < counterContainers.Length; i++)
+            {
+                if (counterContainers[i] != 0)
+                Console.WriteLine($"{container[i]} - {counterContainers[i]}");
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
